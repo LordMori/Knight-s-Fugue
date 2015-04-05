3eef9880e3fda5a42483f897fd838fefab85e68e
Marc Becker <mb1190@ymail.com>
2015-04-04 22:29:10 -0400

    Edited location of some files

---
+++ Knight's Fugue/RogueClass.h
@@ -0,0 +1,14 @@
+//
+//  RogueClass.h
+//  Knight's Fugue
+//
+//  Created by Marc Becker on 3/30/15.
+//  Copyright (c) 2015 Marc Becker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Knight.h"
+
+@interface RogueClass : Knight
+
+@end
