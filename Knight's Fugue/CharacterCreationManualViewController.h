ea058b79a11302da68710379ad8b013e3bc39f24
Marc Becker <mb1190@ymail.com>
2015-04-05 22:17:09 -0400

    Updated look of alert controllers and added two more types of alerts. Removed tab controller from character creation.

---
+++ Knight's Fugue/CharacterCreationManualViewController.h
@@ -0,0 +1,13 @@
+//
+//  CharacterCreationManualViewController.h
+//  Knight's Fugue
+//
+//  Created by Marc Becker on 4/5/15.
+//  Copyright (c) 2015 Marc Becker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterCreationManualViewController : UIViewController
+
+@end
---
+++ Knight's Fugue/CharacterCreationManualViewController.m
@@ -0,0 +1,13 @@
+//
+//  CharacterCreationManualViewController.m
+//  Knight's Fugue
+//
+//  Created by Marc Becker on 4/5/15.
+//  Copyright (c) 2015 Marc Becker. All rights reserved.
+//
+
+#import "CharacterCreationManualViewController.h"
+
+@implementation CharacterCreationManualViewController
+
+@end
